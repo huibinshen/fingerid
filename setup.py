9cfaa0f6aadf25ce10e3eb371c6306a011ca37c5
Huibin Shen <shenh1@pc123.ics.hut.fi>
2014-07-07 09:58:59 +0300

    update metadata

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 config = {
     'description':'fingerid-package',
     'author':'Huibin Shen',
-    'url':'project https://sourceforge.net/projects/fingerid/',
+    'url':'project https://github.com/icdishb/fingerid',
     'author_email':'huibin.shen@aalto.fi',
-    'version':'2.0',
+    'version':'1.4',
     'install_requires':['nose'],
     'packages':find_packages(),
     'name':'fingerid',
